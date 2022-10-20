ca0f65ef89082b2cc349e66e4889badc7198fd9c
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-10-20 18:44:56 +0100

    Update ft_strlcat.c

--- common_core/Libft/ft_strlcat.c
+++ common_core/Libft/ft_strlcat.c
@@ -5,43 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/03 15:58:01 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/04/03 22:09:33 by rbayoumi         ###   ########.fr       */
+/*   Created: 2022/10/19 17:05:13 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/10/20 18:42:32 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
-int	ft_strlen(char *str)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
-{
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	lenght_src;
-	unsigned int	lenght_dest;
+	size_t	i;
+	size_t	j;
+	size_t	lenght_src;
+	size_t	lenght_dst;
 
 	i = 0;
 	j = 0;
-	lenght_src = ft_strlen(src);
-	lenght_dest = ft_strlen(dest);
-	j = lenght_dest;
-	if (size == 0 || size <= lenght_dest)
-		return (lenght_src + size);
-	while (src[i] && i < size - lenght_dest - 1)
+	if (size == 0)
 	{
-		dest[j] = src[i];
+		return (-1);
+	}
+	lenght_src = ft_strlen(src);
+	lenght_dst = ft_strlen(dst);
+	j = lenght_dst;
+	if (size == 0 || size <= lenght_dst)
+		return (lenght_src + size);
+	while (src[i] && i < size - lenght_dst - 1)
+	{
+		dst[j] = src[i];
 		i++;
 		j++;
 	}
-	dest[j] = '\0';
-	return (lenght_dest + lenght_src);
+	dst[j] = '\0';
+	return (lenght_dst + lenght_src);
 }
