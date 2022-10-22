2d378ff6683181a08800b7987702b188d0debc78
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-10-22 10:48:15 +0100

    Update ft_strrchr.c

--- common_core/Libft/ft_strrchr.c
+++ common_core/Libft/ft_strrchr.c
@@ -1,33 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/20 18:52:24 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/10/21 15:13:47 by rbayoumi         ###   ########.fr       */
+/*   Created: 2022/10/20 18:54:36 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/10/21 18:05:57 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
+	int	s_len;
 
-	i=0;
-	while (*(s + i) != '\0')
+	s_len = ft_strlen(s);
+	i = 0;
+	while (s_len >= i)
 	{
-		if (*(s + i) == (char)c)
+		if (*(s + s_len) == (char)c)
 		{
-			return ((char *)(s + i));
+			return ((char *)(s + s_len));
 		}
-		i++;
-	}
-	if ((char)c == '\0')
-	{
-		return ((char *)(s + i));
+		s_len--;
+		if ((char)c == '\0')
+		{
+			return ((char *)(s + s_len));
+		}
 	}
 	return (0);
 }
