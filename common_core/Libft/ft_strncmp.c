4ebcc5c45867730bb8cf05d145cf22078402353b
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-10-22 10:42:45 +0100

    Update ft_strncmp.c

--- common_core/Libft/ft_strncmp.c
+++ common_core/Libft/ft_strncmp.c
@@ -5,22 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/31 14:43:01 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/04/04 18:00:44 by rbayoumi         ###   ########.fr       */
+/*   Created: 2022/10/20 18:55:25 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/10/22 07:57:48 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
+	while ((*(s1 + i) != '\0' || *(s2 + i) != '\0') && i < n)
 	{
-		if (s1[i] > s2[i])
-			return (1);
-		else if (s1[i] < s2[i])
-			return (-1);
+		if (*(unsigned char *)(s1 + i) != *(unsigned char *)(s2 + i))
+		{
+			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+		}
 		i++;
 	}
 	return (0);
