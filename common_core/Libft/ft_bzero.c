9c377092c0d2b18028139fe27d09c84a012e3b1b
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-10-23 11:56:34 +0100

    Update ft_bzero.c

--- common_core/Libft/ft_bzero.c
+++ common_core/Libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 17:36:27 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/10/18 17:49:28 by rbayoumi         ###   ########.fr       */
+/*   Updated: 2022/10/22 09:45:43 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,4 @@ void	ft_bzero(void *s, size_t n)
 		*(unsigned char *)(s + i) = '\0';
 		i++;
 	}
-	return ;
 }
