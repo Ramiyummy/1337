e46e7210707671981aad5958760b92f8feae059e
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-10-23 12:07:55 +0100

    Update ft_memcpy.c

--- common_core/Libft/ft_memcpy.c
+++ common_core/Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:31:37 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/10/19 09:56:34 by rbayoumi         ###   ########.fr       */
+/*   Updated: 2022/10/19 16:09:28 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	{
 		return (NULL);
 	}
-		while (i < n)
-		{
-			*(unsigned char *) (dest + i) = *(unsigned char *) (src + i);
-			i++;
-		}
-
-		return (dest);
+	while (i < n)
+	{
+		*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
+		i++;
+	}
+	return (dest);
 }
