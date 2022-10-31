b881746afc948f24a12d6660afe82cd9da8068ee
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-10-31 09:35:33 +0100

    Create ft_printf.h

---
+++ common_core/ft_printf/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 08:28:58 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/10/31 08:55:48 by rbayoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+
+/* Function prototypes */
+
+void	ft_str(char *s);
+void	ft_char(char c);
+void	ft_nbr(int n);
+
+#endif
