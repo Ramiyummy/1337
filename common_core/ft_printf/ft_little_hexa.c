a89b01c63beb0d55ed5299667f262f6068e94dcf
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-05 18:56:37 +0100

    Create ft_little_hexa.c

---
+++ common_core/ft_printf/ft_little_hexa.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_little_hexa.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 13:11:47 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/11/05 18:17:35 by rbayoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_little_hexa(unsigned int num)
+{
+	int		printed;
+	char	*arr;
+
+	printed = 0;
+	arr = "0123456789abcdef";
+	if (num <= 15)
+	{
+		printed += ft_char(arr[num]);
+	}
+	else
+	{
+		printed += ft_little_hexa(num / 16);
+		printed += ft_little_hexa(num % 16);
+	}
+	return (printed);
+}
