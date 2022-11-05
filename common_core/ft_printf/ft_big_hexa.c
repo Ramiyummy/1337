532ce4d99a2dfff8088972ce22594fbef9724c5e
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-05 18:57:24 +0100

    Create ft_big_hexa.c

---
+++ common_core/ft_printf/ft_big_hexa.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_big_hexa.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 15:30:13 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/11/05 18:17:53 by rbayoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_big_hexa(unsigned int num)
+{
+	int		printed;
+	char	*arr;
+
+	printed = 0;
+	arr = "0123456789ABCDEF";
+	if (num <= 15)
+	{
+		printed += ft_char(arr[num]);
+	}
+	else
+	{
+		printed += ft_big_hexa(num / 16);
+		printed += ft_big_hexa(num % 16);
+	}
+	return (printed);
+}
