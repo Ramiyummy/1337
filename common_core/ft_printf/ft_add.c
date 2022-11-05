6291fc516219bd8c0b83a7a1a4fac090ace8d165
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-05 18:55:44 +0100

    Create ft_add.c

---
+++ common_core/ft_printf/ft_add.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_add.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 16:16:11 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/11/05 18:17:12 by rbayoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_add(unsigned long int num)
+{
+	int		printed;
+	char	*arr;
+
+	arr = "0123456789abcdef";
+	printed = 0;
+	if (num <= 15)
+	{
+		printed += ft_char(arr[num]);
+	}
+	else
+	{
+		printed += ft_add(num / 16);
+		printed += ft_add(num % 16);
+	}
+	return (printed);
+}
