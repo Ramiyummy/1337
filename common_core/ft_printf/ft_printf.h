a311266c7c068dd78e07a0c0ac916afc91e1f235
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-05 18:55:16 +0100

    Update ft_printf.h

--- common_core/ft_printf/ft_printf.h
+++ common_core/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 08:28:58 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/10/31 08:55:48 by rbayoumi         ###   ########.fr       */
+/*   Updated: 2022/11/05 16:16:02 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,18 @@
 # define FT_PRINTF_H
 
 # include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
 
 /* Function prototypes */
 
-void	ft_str(char *s);
-void	ft_char(char c);
-void	ft_nbr(int n);
+int		ft_str(char *s);
+int		ft_char(char c);
+int		ft_nbr(int n);
+int		ft_unsigned_nbr(unsigned int n);
+int		ft_printf(const char *format, ...);
+int		ft_little_hexa(unsigned int num);
+int		ft_big_hexa(unsigned int num);
+int		ft_add(unsigned long int num);
 
 #endif
