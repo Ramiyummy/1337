7631c9d997901c9e0736c3fe6ffec2850c6c9569
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-06 11:05:42 +0100

    Update ft_printf.c

--- common_core/ft_printf/ft_printf.c
+++ common_core/ft_printf/ft_printf.c
@@ -18,22 +18,22 @@ int	check_holders(va_list ptr, const char frmt)
 
 	printed = 0;
 	if (frmt == 'c')
-		printed += ft_char(va_arg(ptr, int));
+		printed = ft_char(va_arg(ptr, int));
 	else if (frmt == 's')
-		printed += ft_str(va_arg(ptr, char *));
+		printed = ft_str(va_arg(ptr, char *));
 	else if (frmt == 'd' || frmt == 'i')
-		printed += ft_nbr(va_arg(ptr, int));
+		printed = ft_nbr(va_arg(ptr, int));
 	else if (frmt == 'u')
-		printed += ft_unsigned_nbr(va_arg(ptr, unsigned int));
+		printed = ft_unsigned_nbr(va_arg(ptr, unsigned int));
 	else if (frmt == '%')
-		printed += ft_char(frmt);
+		printed = ft_char(frmt);
 	else if (frmt == 'x')
-		printed += ft_little_hexa(va_arg(ptr, unsigned int));
+		printed = ft_little_hexa(va_arg(ptr, unsigned int));
 	else if (frmt == 'X')
-		printed += ft_big_hexa(va_arg(ptr, unsigned int));
+		printed = ft_big_hexa(va_arg(ptr, unsigned int));
 	else if (frmt == 'p')
 	{
-		printed += ft_str("0x");
+		printed = ft_str("0x");
 		printed += ft_add(va_arg(ptr, unsigned long int));
 	}
 	return (printed);
