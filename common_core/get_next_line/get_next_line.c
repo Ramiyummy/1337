8c3e2ef656387df105ce15b979ff24e52688e95b
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-18 12:19:03 +0100

    Update get_next_line.c

--- common_core/get_next_line/get_next_line.c
+++ common_core/get_next_line/get_next_line.c
@@ -6,50 +6,58 @@
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 08:26:58 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/11/16 12:34:02 by rbayoumi         ###   ########.fr       */
+/*   Updated: 2022/11/18 11:54:23 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define BUFFER_SIZE 20
 
 char	*get_leftover(int fd, char *leftover)
 {
 	int		nr;
-	char	*buffer;
+	char	buffer[BUFFER_SIZE + 1];
 
-	buffer = (ft_calloc(BUFFER_SIZE, sizeof(char) + 1));
-	if (!buffer)
-		return (NULL);
-	while ( !ft_strchr(leftover, '\n') && nr > 0)
+	nr = 1;
+	while (!ft_strchr(leftover, '\n') && nr > 0)
 	{
 		nr = read(fd, buffer, BUFFER_SIZE);
 		if (nr <= 0)
 		{
-			free(buffer);
 			return (leftover);
 		}
-		//leftover = ft_strjoin(leftover, buffer);
-
-		// read
-		// check if nr less or equal to 0
-		// if yeah free buffer, and return leftover
-		// leftovr join with with leftover and buffer,
-		// free it
-		// return leftover
+		buffer[nr] = '\0';
+	    leftover = ft_strjoin(leftover, buffer);
 	}
-	free(buffer);
 	return (leftover);
 }
 char	*get_next_line(int fd)
 {
-	char			dline;
+	char			*dline;
 	char static		*leftover;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-//	leftover = get_leftover(fd, leftover);
-//	line = get_dline(fd, line);
-//	leftover = clean_leftover(fd, leftover);
-//	return (dline);
+	leftover = get_leftover(fd, leftover);
+	dline = get_dline(leftover);
+	printf("%s\n", leftover);
+	//leftover = clean_leftover(leftover);
+	return (dline);
+}
+
+int main(void)
+{
+	int fd, i;
+	char	*buffer;
+
+	fd = open("rami.txt", O_RDONLY);
+
+	buffer = get_next_line(fd);
+	i = 0;
+	while (i < 2)
+	{
+	printf("%s", buffer);
+	i++;
+	}
+
+	return (0);
 }
