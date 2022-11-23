28dd87bdff1342266352b8126b2ba0bfba916f4f
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-23 15:58:39 +0100

    Update get_next_line.c

--- common_core/get_next_line/get_next_line.c
+++ common_core/get_next_line/get_next_line.c
@@ -5,12 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/07 08:26:58 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/11/22 17:21:16 by rbayoumi         ###   ########.fr       */
+/*   Created: 2022/11/23 08:32:12 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/11/23 15:51:51 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*first_draft(int fd, char *f_draft)
 {
@@ -22,39 +23,37 @@ char	*first_draft(int fd, char *f_draft)
 	{
 		nr = read(fd, buffer, BUFFER_SIZE);
 		if (nr <= 0)
-		{
 			return (f_draft);
-		}
 		buffer[nr] = '\0';
-	    f_draft = ft_strjoin(f_draft, buffer);
-		printf("%s", f_draft);
+		f_draft = ft_strjoin(f_draft, buffer);
 	}
 	return (f_draft);
 }
 
 char	*get_next_line(int fd)
 {
-	//char			*dline;
+	char			*dline;
 	char static		*f_draft;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	f_draft = first_draft(fd, f_draft);
-	//dline = get_dline(f_draft);
-	printf("%s\n", f_draft);
-	//f_draft = final_draft(f_draft);
+	dline = get_dline(f_draft);
+//	f_draft = final_draft(f_draft);
 	return (f_draft);
 }
+#include <stdio.h>
 
 int main(void)
 {
-	int fd;
-	char	*buffer;
+	int	fd, i;
 
+	i = 0;
 	fd = open("rami.txt", O_RDONLY);
-
-	buffer = get_next_line(fd);
-	printf("%s", buffer);
-
-	return (0);
+	while (i < 1)
+	{
+		printf("%s", get_next_line(fd));
+		i++;
+	}
+	return 0;
 }
