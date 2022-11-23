64731cbc3b49b2503398eeafb9c27ee770dccdce
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-23 17:39:58 +0100

    Update get_next_line.h

--- common_core/get_next_line/get_next_line.h
+++ common_core/get_next_line/get_next_line.h
@@ -6,29 +6,27 @@
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 08:28:03 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/11/16 12:28:14 by rbayoumi         ###   ########.fr       */
+/*   Updated: 2022/11/23 15:42:00 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
+# include <unistd.h>
 # include <fcntl.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 4
-#endif
+#  define BUFFER_SIZE 8
+# endif
 
 /* Functions prototypes */
 
 char	*get_next_line(int fd);
-void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
-void	ft_bzero(void *s, size_t n);
-char	*ft_strjoin(char const *father, char const *mother);
-
+char	*ft_strjoin(char *father, char *mother);
+char	*get_dline(char	*f_draft);
+char	*final_draft(char *f_draft);
 #endif
