f5de3f87a7219a17042be9f9d3149d105c5864f5
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-24 15:36:28 +0100

    Update get_next_line.c

--- common_core/get_next_line/get_next_line.c
+++ common_core/get_next_line/get_next_line.c
@@ -6,12 +6,26 @@
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 08:32:12 by rbayoumi          #+#    #+#             */
-/*   Updated: 2022/11/24 13:00:02 by rbayoumi         ###   ########.fr       */
+/*   Updated: 2022/11/24 15:33:33 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+
+
+
+char *handle_read_error(int nr, char *first_d, char *buffer)
+{
+	if (nr < 0)
+	{
+		free(first_d);
+		free(buffer);
+		return (NULL);
+	}
+	free(buffer);
+	return (first_d);
+}
 
 char	*first_draft(int fd, char *first_d)
 {
@@ -26,10 +40,7 @@ char	*first_draft(int fd, char *first_d)
 	{
 		nr = read(fd, buffer, BUFFER_SIZE);
 		if (nr <= 0)
-		{
-			free(buffer);
-			return (first_d);
-		}
+			return (handle_read_error(nr, first_d, buffer));
 		buffer[nr] = '\0';
 		first_d = ft_strjoin(first_d, buffer);
 	}
@@ -43,22 +54,11 @@ char	*get_next_line(int fd)
 	static char		*first_d;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
 		return (NULL);
+	}
 	first_d = first_draft(fd, first_d);
 	dline = get_dline(first_d);
 	first_d = final_draft(first_d);
 	return (dline);
-}
-int main(void)
-{
-	int	fd;
-	int	i;
-
-	i = 0;
-	fd = open("rami.txt", O_RDONLY);
-	while (i < 6)
-	{
-		printf("%s", get_next_line(fd));
-		i++;
-	}
 }
