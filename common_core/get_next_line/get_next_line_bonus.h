c4fa18ed9736e217fcec688f3b66050c3418c67b
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-11-24 16:20:40 +0100

    Update get_next_line_bonus.h

--- common_core/get_next_line/get_next_line_bonus.h
+++ common_core/get_next_line/get_next_line_bonus.h
@@ -1,1 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 15:46:44 by rbayoumi          #+#    #+#             */
+/*   Updated: 2022/11/24 15:56:40 by rbayoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8
+# endif
+
+/* Functions prototypes */
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *father, char *mother);
+char	*get_dline(char	*f_draft);
+char	*final_draft(char *f_draft);
+
+#endif
