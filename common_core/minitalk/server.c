458cd30609d18ed0dc749f9e8f959052d3ce35f8
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2023-01-10 15:38:16 +0100

    Update server.c

--- common_core/minitalk/server.c
+++ common_core/minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 16:23:45 by rbayoumi          #+#    #+#             */
-/*   Updated: 2023/01/07 18:41:13 by rbayoumi         ###   ########.fr       */
+/*   Updated: 2023/01/10 14:58:53 by rbayoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 void	sig_handler(int sig)
 {
-	static int bit;
-	static int bit_message;
-	int mask;
-
-	mask = 0b10000000;
+	static int	bit;
+	static int	message;
 
 	if (sig == SIGUSR1)
-		bit_message |= mask;
-	mask >>= 1;
-	if (mask == 0b00000001)
+		message |= (0b1 << bit);
+	bit++;
+	if (bit == 8)
 	{
-		printf("%c", bit_message);
-		mask = 0b10000000;
-		bit_message = 0;
+		ft_char(message);
+		bit = 0;
+		message = 0;
 	}
 }
 
-int main(void)
+int	main(void)
 {
-	int pid;
+	int	pid;
 
 	pid = getpid();
-	printf("PID: %d\n", pid);
+	ft_str("PID: ");
+	ft_nbr(pid);
+	ft_str("\nYou are the love of my heart, ");
+	ft_str("the one my heart desires, I will not mind waiting for you...\n");
 	while (1)
 	{
 		signal(SIGUSR1, sig_handler);
