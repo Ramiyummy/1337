1280920c4dcc60e52063e1263b3c32894a6a4e0f
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2023-01-10 15:47:06 +0100

    Update minitalk.h

--- common_core/minitalk/minitalk.h
+++ common_core/minitalk/minitalk.h
@@ -1,7 +1,19 @@
-#ifdef MINITALK_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 16:28:38 by rbayoumi          #+#    #+#             */
+/*   Updated: 2023/01/10 14:10:51 by rbayoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
 # define MINITALK_H
-#endif
 
 #include <signal.h>
-#include "libft/headers/libft.h"
-#include "libft/headers/ft_printf.h"
+#include "libft/libft.h"
+
+#endif
