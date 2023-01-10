27a528939cdda07da93c745ada5530cca0f12d96
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2023-01-10 15:46:15 +0100

    Create libft.h

---
+++ common_core/minitalk/libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 11:01:20 by rbayoumi          #+#    #+#             */
+/*   Updated: 2023/01/10 15:45:41 by rbayoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+/* Libc functions prototypes */
+
+int		ft_atoi(const char *nptr);
+int		ft_char(char c);
+void	ft_str(char *s);
+int		ft_nbr(int n);
+int		ft_isdigit(int c);
+
+#endif
