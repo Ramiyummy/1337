23decc499066b2137134e9b5712dcd29e7c1e06d
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2023-01-14 12:41:24 +0100

    Create actions.c

---
+++ common_core/push_swap/actions.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbayoumi <rbayoumi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 17:01:04 by rbayoumi          #+#    #+#             */
+/*   Updated: 2023/01/12 17:03:54 by rbayoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	swap_a(int *random)
+{
+
+}
+
+void	swap_b(int *random)
+{
+
+}
+
+void	swap_ab(int *random)
+{
+
+}
+
+void	push_a(int *random)
+{
+
+}
+
+void	push_b(int *random)
+{
+
+}
+
+void	push_ab(int *random)
+{
+
+}
